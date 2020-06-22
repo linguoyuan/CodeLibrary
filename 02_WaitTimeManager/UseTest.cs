b6d8763ca3867db02454f34687007d846a0ebf42
lgy_lenovo <19977439879@163.com>
2020-06-22 21:04:19 +0800

    WaitTimeManager

---
+++ 02_WaitTimeManager/UseTest.cs
@@ -0,0 +1,10 @@
+﻿
+
+//3秒后执行
+Coroutine coroutine = WaitTimeManager.WaitTime(3, () =>
+{
+   //to do
+});
+
+//等待过程中执行以下语句，则取消执行
+WaitTimeManager.CancelWait(ref coroutine);
---
+++ 02_WaitTimeManager/WaitTimeManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitTimeManager
+{
+    private static TaskBehaviour m_Task;
+
+    static WaitTimeManager()
+    {
+        GameObject go = new GameObject("WaitTimeManager");
+        GameObject.DontDestroyOnLoad(go);
+        m_Task = go.AddComponent<TaskBehaviour>();
+    }
+
+
+    public static void CancelWait(ref Coroutine coroutine)
+    {
+        if (coroutine != null)
+        {
+            m_Task.StopCoroutine(coroutine);
+        }
+    }
+
+    public static Coroutine WaitTime(float time, UnityAction callback)
+    {
+        return m_Task.StartCoroutine(Coroutine(time, callback));
+    }
+
+    private static IEnumerator Coroutine(float time, UnityAction callback)
+    {
+        yield return new WaitForSeconds(time);
+        if (callback != null)
+        {
+            callback();
+        }
+    }
+
+    //内部类
+    class TaskBehaviour : MonoBehaviour
+    {
+    }
+}
